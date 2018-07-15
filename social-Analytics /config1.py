f64dc729a6377b87a60e9bf98bc965e2fca402db
Belay Tegegn <belaytegegn@Belays-MacBook-Pro.local>
2018-07-15 19:19:40 -0400

    social analytics

---
+++ social-Analytics /config1.py
@@ -0,0 +1,9 @@
+consumer_key = '0gNvW3tTD4VNNs9F5BN8Y8aej'
+
+consumer_secret = 'RDBzocYSmzymp98H340LDPVIDRVEgfRJvkDh69NFNufCSMBhmY'
+
+
+access_token ='1013062013373820928-uC4zT1ds1pjOo8jIfVpuE4OHXySUb9'
+
+
+access_token_secret ='Qe094f079V8UxmJACjgTo8bfVijU5wHXmrQPUZc1OBcyX'
